839edc5703b8efbc05a05961f73532ddc376ced5
darrylmorton <darryl.j.morton@gmail.com>
2024-08-14 07:12:35 +0100

    correct version bump

--- tests/unit/utils/app_util_test.py
+++ tests/unit/utils/app_util_test.py
@@ -5,4 +5,4 @@ class TestAppUtil:
     async def test_get_app_version(self):
         actual_result = AppUtil.get_app_version()
 
-        assert actual_result == "1.0.3"
+        assert actual_result == "1.0.1"
