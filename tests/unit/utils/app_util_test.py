f6f8dac760d5b17e1649cf96cfc06d5eafd8384d
darrylmorton <darryl.j.morton@gmail.com>
2025-08-13 00:27:11 +0100

    feat: version bump

--- tests/unit/utils/app_util_test.py
+++ tests/unit/utils/app_util_test.py
@@ -7,7 +7,7 @@ class TestAppUtil:
     def test_get_app_version(self):
         actual_result = AppUtil.get_app_version()
 
-        assert actual_result == "1.0.6"
+        assert actual_result == "2.0.0"
 
     @skip
     def test_set_openapi_info(self):
